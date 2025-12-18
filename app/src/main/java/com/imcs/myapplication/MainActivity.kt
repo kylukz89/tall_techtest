da44f7d6c3018d7ea0ec3f9349e03e5de160a5ac
Igor Maximo <igormaximo_1989@hotmail.com>
2025-12-18 14:18:27 -0300

    feat: base activity

--- app/src/main/java/com/imcs/myapplication/MainActivity.kt
+++ app/src/main/java/com/imcs/myapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.imcs.myapplication
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private val searchCityBar: AppCompatEditText by lazy {
+        findViewById(R.id.city_search_name)
+    }
+
+    override fun setContentLayout(): Int = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun setupViews() {
 
     }
 }
